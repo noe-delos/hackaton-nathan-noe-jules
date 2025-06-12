25417c0ca1f8216e2ce5bff6cd35ad53ca6a236a
Jules Candau-Tilh <jules.candautilh@gmail.com>
2025-06-12 17:22:55 +0200

    added gif response

--- backend/answer_with_gif.py
+++ backend/answer_with_gif.py
@@ -1,10 +1,33 @@
+import json
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPEN_AI_KEY"))
+
+def call_to_giphy(query):
+    url = "https://api.giphy.com/v1/gifs/search"
+    params = {
+        "api_key": os.getenv("GIPHY_KEY"),
+        "q": query,
+        "limit": 1,
+        "offset": 0,
+        "rating": "g",
+        "bundle": "messaging_non_clips"
+    }
+    response = requests.get(url, params=params)
+    data = response.json().get('data', [])
+    gifs = []
+    for gif in data:
+        gifs.append({
+            'gif_url': gif['images']['original']['url'],
+            'giphy_url': gif['url'],
+            'embed_url': gif['embed_url']
+        })
+    return gifs
 
 def gif_answer(conversation):
     """
@@ -14,14 +37,20 @@ def gif_answer(conversation):
     last_message = conversation[-1]['content']
 
     response = client.chat.completions.create(
-        model="gpt-4o",  # or "gpt-4-turbo"
+        model="gpt-4o",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f"Conversation: {conversation}\n\nPlease respond to the last message: '{last_message}'"}
-        ]
+            {"role": "user", "content": f"""Here is the conversation: {conversation}\n\n
+            Please answer with a json containing the keywords to send to search the giphy API to answer the last message with a gif: '{last_message}'
+            The JSON should be like this: {{"keywords": ["keyword1 keyword2 keyword3"]}}"""}
+        ],
+        response_format={"type": "json_object"}
     )
 
-    return response.choices[0].message.content
+    json_response = json.loads(response.choices[0].message.content)
+    giphy_response = call_to_giphy(json_response.get("keywords"))
+
+    return giphy_response
 
 
 if __name__ == "__main__":
